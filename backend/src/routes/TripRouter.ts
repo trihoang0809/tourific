3720467cbd571e9b0b810473e946336e1b1b67ca
Tri Hoang <trihoang0809@gmail.com>
2024-04-06 01:22:35 -0500

    get ongoing, upcoming, and past trips

--- backend/src/routes/TripRouter.ts
+++ backend/src/routes/TripRouter.ts
@@ -1,16 +1,74 @@
-import express from 'express';
-import { PrismaClient } from '@prisma/client';
+import express from "express";
+import { PrismaClient } from "@prisma/client";
 
 const router = express.Router();
 const prisma = new PrismaClient();
 
-// Get all trips
-router.get('/', async (req, res) => {
+// Get trips
+
+router.get("/", async (req, res) => {
   try {
-    const trips = await prisma.trip.findMany();
+    let queryConditions = {};
+    const now = new Date();
+
+    if (req.query.ongoing === "true") {
+      queryConditions = {
+        where: {
+          AND: [
+            {
+              startDate: {
+                lt: now,
+              },
+            },
+            {
+              endDate: {
+                gt: now,
+              },
+            },
+          ],
+        },
+      };
+    } else if (req.query.past === "true") {
+      queryConditions = {
+        where: {
+          endDate: {
+            lt: now,
+          },
+        },
+      };
+    } else if (req.query.upcoming === "true") {
+      queryConditions = {
+        where: {
+          startDate: {
+            lt: now,
+          },
+        },
+      };
+    }
+
+    const trips = await prisma.trip.findMany(queryConditions);
     res.json(trips);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "An error occurred while fetching trips." });
+  }
+});
+
+router.post("/", async (req, res) => {
+  const { name, startDate, endDate, location } = req.body;
+  try {
+    const trip = await prisma.trip.create({
+      data: {
+        name,
+        startDate,
+        endDate,
+        location,
+      },
+    });
+    res.status(201).json(trip);
   } catch (error) {
-    res.status(500).json({ error: 'An error occurred while fetching trips.' });
+    console.log(error);
+    res.status(500).json({ error: "An error occurred while creating the trip." });
   }
 });
 
