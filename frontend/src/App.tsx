474e356c98f8c09f49e69cb5bec66fe50b4101a4
tranguv <89886131+tranguv@users.noreply.github.com>
2024-04-08 00:43:52 -0400

    structure frontend folder

--- frontend/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { HomeScreen } from "./screens/HomeScreen";
-
-const App: React.FC = () => {
-  return <HomeScreen />;
-};
-
-export default App;
-
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,13 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+
 module.exports = {
-  content: [],
+  content: [
+    "./src/app/index.{js,jsx,ts,tsx}",
+    "./src/app/**/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
