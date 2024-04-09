676889519f30bea0ed35f35e820cc6520c4a7f69
tranguv <89886131+tranguv@users.noreply.github.com>
2024-04-09 00:39:07 -0400

    remove test folder

--- frontend/src/app/test/_layout.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'; // This import is often required for JSX transformation.
-import { Stack, Tabs } from 'expo-router';
-import { View } from 'react-native';
-
-const Layout = () => {
-  return <Tabs />;
-}
-
-export default Layout;
