92535824e5a68c0e9daead247bb89f5f7027a2d4
Tri Hoang <trihoang0809@gmail.com>
2024-04-23 00:46:41 -0500

    add notification icon

--- frontend/src/components/HomeScreenHeader.tsx
+++ frontend/src/components/HomeScreenHeader.tsx
@@ -1,11 +1,26 @@
-import { View, Text, Image, StyleSheet, SafeAreaView } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
 import { UserProps } from "../types";
+import { Ionicons } from "@expo/vector-icons";
 
 export const HomeScreenHeader: React.FC<UserProps> = ({ user }) => {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.content}>
         <Text style={styles.appName}>tourific</Text>
+        <TouchableOpacity
+          onPress={() => {
+            /* navigate to notifications */
+          }}
+        >
+          <Ionicons name="notifications-outline" size={24} color="black" />
+        </TouchableOpacity>
         <Image style={styles.avatar} source={{ uri: user.avatar.url }} />
       </View>
     </SafeAreaView>
@@ -20,6 +35,11 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between", // Aligns children at space-between
     alignItems: "center",
+    padding: 10,
+  },
+  notificationIcon: {
+    position: "absolute",
+    right: 60,
     padding: 10,
   },
   appName: {
