3369a411edf4fc2159ff6fbb1875a140a8f87314
Duy Kha <duykha121314888@gmail.com>
2024-04-24 18:29:21 -0500

    Update App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { HomeScreen } from "./screens/HomeScreen";
-
+import { ListFilteredCards } from "./screens/UpcomingList";
 const App: React.FC = () => {
-  return <HomeScreen />;
+  // return <HomeScreen />;
+  return <ListFilteredCards />;
 };
 
 export default App;
