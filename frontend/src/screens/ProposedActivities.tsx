14c968a459398227e1e09b5af0dabf43cca89e34
Duy Kha <duykha121314888@gmail.com>
2024-04-26 20:52:38 -0500

    Set up page

--- frontend/src/screens/ProposedActivities.tsx
+++ frontend/src/screens/ProposedActivities.tsx
@@ -1,7 +1,34 @@
-import {View}  from "react-native";
+import {
+  View,
+  Text,
+  StatusBar,
+  TextInput,
+}  from "react-native";
+import { styled } from 'nativewind';
+import { withExpoSnack } from 'nativewind';
+import React from 'react';
+
+// const StyledView = styled(View);
+// const StyledText = styled(Text);
+
+const Header = () => (
+  <View className="flex-1 items-center justify-center bg-red">
+      <Text>Open up App.js to start working on your app!</Text>
+  </View>
+);
+
+
 
 export const ProposedActivities: React.FC = () => {
   return(
-    <View></View>
+    // <StyledView>
+    //   <StatusBar backgroundColor="black"/>
+    //   <Header></Header>
+    //   <Text>Haha</Text>
+    // </StyledView>
+  <View className="flex-1 items-center justify-center bg-red">
+    <Header></Header>
+      <Text >Open up App.js to start working on your app!</Text>
+    </View>
   );
 };
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./App.{js,jsx,ts,tsx}", "./<custom directory>/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
