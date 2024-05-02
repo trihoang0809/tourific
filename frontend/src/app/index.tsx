7effe0e115fd58fa4829978b123c126be182f76f
Van Nguyen <87792873+yourvivian@users.noreply.github.com>
2024-05-02 01:09:16 -0700

    Update link to trip page

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -7,9 +7,6 @@ import { Link } from 'expo-router';
 export default function App() {
   return (
     <View className="flex-1 bg-white items-center justify-center">
-      <Text>Open up app/index.tsx to start working on your app!</Text>
-      <Link href='/trips/2'>Go to trip detail page</Link>
-      <Link href='/trips/661f78b88c72a65f2f6e49d4/activities'>Go to mock activities</Link>
       <Link href='/trips/661f78b88c72a65f2f6e49d4'>Go to trip at the beach</Link>
       <Link href='/trips/create'>Go to trip create</Link>
       <StatusBar style="auto" />
