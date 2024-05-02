2ecf1513a5181a65c2c53c201cee13d9097b51b5
Van Nguyen <87792873+yourvivian@users.noreply.github.com>
2024-05-02 01:08:10 -0700

    Update header within a trip

--- frontend/src/app/trips/[id]/(tabs)/_layout.tsx
+++ frontend/src/app/trips/[id]/(tabs)/_layout.tsx
@@ -2,15 +2,39 @@ import React from 'react';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { AntDesign } from '@expo/vector-icons';
 import { Tabs } from 'expo-router';
+import { Link, Stack, useLocalSearchParams } from "expo-router";
+import { Feather, Ionicons } from "@expo/vector-icons";
+import { View } from 'react-native'
 
 export default function TabLayout() {
+  const { id } = useLocalSearchParams();
+
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: 'blue', headerShown: false }}>
+    <Tabs screenOptions={{ tabBarActiveTintColor: 'navy', headerShown: false }}>
       <Tabs.Screen
         name="index"
         options={{
           title: 'Home',
           tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
+          headerShown: true, 
+          headerTransparent: true,
+          headerTitle: '',
+          headerRight: () => (
+            <Link href={`/trip/create?id=${id}`}>
+              <View style={{
+                backgroundColor: 'rgba(0, 0, 128, 0.3)', // This sets the background color to black with 10% opacity
+                borderRadius: 50,
+                padding: 5,
+              }}>
+                <Feather
+                  // onPressIn={showMoreSetting}
+                  // onPressOut={notShowMoreSetting}
+                  name="edit-2"
+                  size={24}
+                  color="black" />
+              </View>
+          </Link>
+          ),
         }}
       />
       <Tabs.Screen
