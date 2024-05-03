b802d99ffd7fa270018cc7eebeca1a4928416cad
Van Nguyen <87792873+yourvivian@users.noreply.github.com>
2024-05-03 14:06:39 -0700

    Make calendar start from trip's startdate

--- frontend/src/app/trips/[id]/(tabs)/itinerary.tsx
+++ frontend/src/app/trips/[id]/(tabs)/itinerary.tsx
@@ -1,13 +1,18 @@
-import React, {Component, useState, useEffect} from 'react';
-import { MaterialIcons } from '@expo/vector-icons';
-import { router } from 'expo-router';
-import {Alert, StyleSheet, Text, View, TouchableOpacity} from 'react-native';
-import {Agenda, DateData, AgendaEntry, AgendaSchedule} from 'react-native-calendars';
+import React, { Component, useState, useEffect } from "react";
+import { MaterialIcons } from "@expo/vector-icons";
+import { router } from "expo-router";
+import { Alert, StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import {
+  Agenda,
+  DateData,
+  AgendaEntry,
+  AgendaSchedule,
+} from "react-native-calendars";
 // import testIDs from '../testIDs';
-import AgendaList from './../../../../components/Agenda';
-import { useRoute } from '@react-navigation/native';
+import AgendaList from "./../../../../components/Agenda";
+import { useRoute } from "@react-navigation/native";
 import { Link, Stack, useGlobalSearchParams } from "expo-router";
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Itinerary = () => {
   const { id } = useGlobalSearchParams();
@@ -19,7 +24,7 @@ const Itinerary = () => {
     startMinute: 0,
   });
 
-  const getTrip = async ({ id: text }: { id: string; }) => {
+  const getTrip = async ({ id: text }: { id: string }) => {
     try {
       const response = await fetch(`http://localhost:3000/trips/${id}`, {
         method: "GET",
@@ -42,36 +47,7 @@ const Itinerary = () => {
     getTrip({ id });
   }, []);
 
-  return (
-    <View>
-      <Stack.Screen
-        options={{
-          title: 'Iternary',
-          headerShown: true,
-          headerStyle: {
-            backgroundColor: 'white',
-          },
-          headerTransparent: false,
-          headerLeft: () => (
-            <MaterialIcons
-              name="arrow-back"
-              size={24}
-              color="black"
-              style={{ marginLeft: 10 }}
-              onPress={() => router.navigate('/')}
-            />
-          ),
-        }}
-      />
-      <SafeAreaView>
-        <View>
-          <Text>
-            Itinerary
-          </Text>
-        </View>
-      </SafeAreaView>
-    </View>
-  );
-}
+  return <AgendaList />;
+};
 
 export default Itinerary;
