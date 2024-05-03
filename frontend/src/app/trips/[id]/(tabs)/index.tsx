20c0c158d5b1a0c0ca13abecdd421b1b353ee412
Van Nguyen <87792873+yourvivian@users.noreply.github.com>
2024-05-03 12:42:23 -0700

    Update global search param

--- frontend/src/app/trips/[id]/(tabs)/index.tsx
+++ frontend/src/app/trips/[id]/(tabs)/index.tsx
@@ -1,14 +1,14 @@
 import { View, Text, ScrollView, Image } from "react-native";
 import React, { useState, useEffect } from "react";
 import favicon from "@/assets/favicon.png";
-import { useLocalSearchParams } from "expo-router";
-import { Ionicons } from "@expo/vector-icons";
+import { Link, Stack, useGlobalSearchParams } from "expo-router";
+import { Feather, Ionicons } from "@expo/vector-icons";
 import { Dimensions } from "react-native";
 import { DateTime } from "luxon";
 import { StyleSheet } from "react-native";
 
 const TripDetailsScreen = () => {
-  const { id } = useLocalSearchParams();
+  const { id } = useGlobalSearchParams();
   // const id = parseFloat(typeof idString === 'string' ? idString : idString[0]);
 
   // const trip = trips.find(trip => trip.id === id);
--- frontend/src/app/trips/[id]/(tabs)/itinerary.tsx
+++ frontend/src/app/trips/[id]/(tabs)/itinerary.tsx
@@ -1,6 +1,21 @@
-import { View, Text } from 'react-native';
+import React, {Component} from 'react';
+import {Alert, StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import {Agenda, DateData, AgendaEntry, AgendaSchedule} from 'react-native-calendars';
+// import testIDs from '../testIDs';
+import AgendaList from './../../../../components/Agenda';
+import { useRoute } from '@react-navigation/native';
+import { Link, Stack, useGlobalSearchParams } from "expo-router";
 
 const Itinerary = () => {
+  const { id } = useLocalSearchParams();
+  console.log("id calendar", id);
+  // console.log(window.location.href); // logs the current URL
+  // const route = useRoute();
+  // const { id } = this.props.route.params.id as { id: string };
+  // console.log(route.name, id);
+  // const location = useLocation();
+  // console.log(location.pathname);
+
   return (
     <View>
       <Text>Itinerary</Text>
