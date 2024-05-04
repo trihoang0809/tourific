79c23b65469fad01566eb8c16e9f71529438e5b1
Van Nguyen <87792873+yourvivian@users.noreply.github.com>
2024-05-03 18:11:44 -0700

    Restyle tab design

--- frontend/src/app/trips/[id]/(tabs)/_layout.tsx
+++ frontend/src/app/trips/[id]/(tabs)/_layout.tsx
@@ -4,13 +4,25 @@ import { AntDesign } from '@expo/vector-icons';
 import { Tabs } from 'expo-router';
 import { Link, Stack, useLocalSearchParams } from "expo-router";
 import { Feather, Ionicons } from "@expo/vector-icons";
-import { View } from 'react-native'
+import { View, Text } from 'react-native'
 
 export default function TabLayout() {
   const { id } = useLocalSearchParams();
 
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: 'navy', headerShown: false }}>
+    <Tabs screenOptions={{ 
+      tabBarActiveTintColor: '#006ee6', 
+      headerShown: false, 
+      tabBarStyle : {
+        height: 70, // Adjust this value as needed
+        justifyContent: 'center',
+        paddingVertical: 5,
+      },
+      tabBarLabelStyle: {
+        fontSize: 12,
+        marginBottom: 5
+      },
+    }}>
       <Tabs.Screen
         name="index"
         options={{
@@ -22,16 +34,19 @@ export default function TabLayout() {
           headerRight: () => (
             <Link href={`/trip/create?id=${id}`}>
               <View style={{
-                backgroundColor: 'rgba(0, 0, 128, 0.3)', // This sets the background color to black with 10% opacity
+                backgroundColor: '#004999', // This sets the background color to black with 10% opacity
                 borderRadius: 50,
-                padding: 5,
+                padding: 10,
+                flexDirection: 'row',
+                alignItems: 'center',
               }}>
                 <Feather
                   // onPressIn={showMoreSetting}
                   // onPressOut={notShowMoreSetting}
                   name="edit-2"
-                  size={24}
-                  color="black" />
+                  size={20}
+                  color="white" />
+                <Text style={{marginLeft:10, color:"white"}}>Edit</Text>
               </View>
           </Link>
           ),
