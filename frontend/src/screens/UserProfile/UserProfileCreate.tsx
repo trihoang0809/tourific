b2235a18830eaee14aa6b3b0699c6d72f5e04b74
Duy Kha <duykha121314888@gmail.com>
2024-05-08 07:59:33 -0500

    Update Create User Profile UI

--- frontend/src/screens/UserProfile/UserProfileCreate.tsx
+++ frontend/src/screens/UserProfile/UserProfileCreate.tsx
@@ -17,6 +17,7 @@ import { useState, useEffect } from "react";
 import DateTimePickerModal from "react-native-modal-datetime-picker";
 import * as ImagePicker from 'expo-image-picker';
 import { User } from "@/types";
+import { AntDesign, Ionicons } from "@expo/vector-icons";
 
 interface editProps {
   method: string;
@@ -27,6 +28,11 @@ export const UserProfileCreate: React.FC<editProps> = ({
   method,
   id = "",
 }) => {
+  const validColor = "green";
+  const invalidColor = "red";
+  const noInputColor = "black";
+
+
   //Declare useState
   const [userName, setUserName] = useState("");
   const [firstName, setFirstName] = useState("");
@@ -38,7 +44,13 @@ export const UserProfileCreate: React.FC<editProps> = ({
   const [isDOBDatePickerVisible, setDOBDatePickerVisibility] = useState(false);
   const [isFormFilled, setIsFormFilled] = useState(false);
   const [userProfile, setUserProfile] = useState<User>();
-
+  const [title, setTitle] = useState("Create Your Profile");
+  const [userNameValid, setUserNameValid] = useState(noInputColor);
+  const [firstNameValid, setFirstNameValid] = useState(noInputColor);
+  const [lastNameValid, setLastNameValid] = useState(noInputColor);
+  const [passwordValid, setPasswordValid] = useState(noInputColor);
+  const [DOBValid, setDOBValid] = useState(noInputColor);
+  const [hidePassword, setHidePassword] = useState(true);
   // Determine method
   useEffect(() => {
     // Let ios as default
@@ -48,6 +60,9 @@ export const UserProfileCreate: React.FC<editProps> = ({
       serverUrl = 'http://10.0.2.2:3000'
 
     if(method === "PUT"){
+      setTitle("Edit");
+
+
       const getData = async () => {
         try {
           const link = serverUrl + "/" + id;
@@ -85,30 +100,52 @@ export const UserProfileCreate: React.FC<editProps> = ({
       }
     ])
   };
-
-
-  const onPressBack = () => {
-    console.log("You pressed on Return Button");
-    if(!isSaved)
-    {
-      alertUnSaved();
-    }
-  };
   
   const Header = () => (
     <View>
       <StatusBar backgroundColor="black"/>
       <View style={styles.headerContainer}>
-        <Text style={[material.display1, {alignSelf: "center"}]}>Create User Profile</Text>
+        <Text style={[material.display1, {alignSelf: "center"}]}>{title}</Text>
       </View>
     </View>
   );
 
 
+  //Validate User input
+  const validateUserName = (value: String) => {
+    if(value.length >= 5)
+      setUserNameValid(validColor);
+    else setUserNameValid(invalidColor);
+  }
+
+  const validateFirstName = (value: String) => {
+    console.log(firstName.length);
+    console.log(firstName);
+    if(value.length >= 1)
+      setFirstNameValid(validColor);
+    else setFirstNameValid(invalidColor);
+  }
+
+  const validateLastName = (value: String) => {
+    if(value.length >= 1)
+      setLastNameValid(validColor);
+    else setLastNameValid(invalidColor);
+  }
+
+  const validatePassword = (value: String) => {
+    if(value.length >= 8)
+      setPasswordValid(validColor);
+    else setPasswordValid(invalidColor);
+  }
+
+  const validateDOB = () => {
+    setDOBValid(validColor);
+  }
+
   //Submit Button
   const validateForm = () => {
     // console.log(activityDescription);
-    if(userName !== "" && firstName !== "" && lastName !== "" && password !== "")
+    if(userNameValid && firstNameValid && lastNameValid && passwordValid)
       setIsFormFilled(true);
   };
 
@@ -155,14 +192,12 @@ export const UserProfileCreate: React.FC<editProps> = ({
 
 
   const SubmitButton = () => (
-    <TouchableWithoutFeedback onPress={onPressSubmit}>
-      <View style={styles.submitButton}>
-        <Text style={[material.headline, {color: "black"}]}>Submit</Text>
-      </View>
-    </TouchableWithoutFeedback>
+    <Pressable onPress={onPressSubmit} style={styles.submitButton}>
+        <Text style={[material.headline, {color: "black"}]}>Create Account</Text>
+    </Pressable>
   );
 
-  //Show Start || End Date Picker Modal
+  //Show DOB Date Picker Modal
   const onPressShowDOBDatePicker = () => {
     setDOBDatePickerVisibility(true);
   };
@@ -212,64 +247,115 @@ export const UserProfileCreate: React.FC<editProps> = ({
       {/* Form */}
       <View style={styles.formInputContainer}>
         {/* Upload avatar */}
-        <View>
-          <Pressable onPress={pickImage}>
+        <View style={{marginBottom: 20,}}>
+          <Pressable onPress={pickImage} style={styles.avatar}>
             <Image source={{ uri: avatar.url }} style={styles.avatar} />
           </Pressable>
         </View>
 
         {/* userName input  */}
         <View style={styles.queInput}>
-          <Text style={[material.headline, {color: "black", marginBottom: 10,}]}>User Name:</Text>
-          <TextInput 
-            onChangeText={(value) => {setUserName(value)}} 
-            style={[material.title, styles.formInput]} 
-            placeholder="Undefined" 
-            value={userName}
-          >
-          </TextInput>
+          <Text style={[material.title, {color: "grey", marginBottom: 0,}]}>Username</Text>
+          <View style={{flexDirection: "row"}}>
+            <TextInput 
+              onChangeText={(value) => {setUserName(value); validateUserName(value)}} 
+              style={[material.title, styles.formInput,  {borderColor: userNameValid, flex: 1}]} 
+              placeholder="Enter username" 
+              placeholderTextColor="#aaa"
+              value={userName}
+            >
+            </TextInput>
+            <View style={{alignItems:"flex-end"}}>
+                {
+                  userNameValid === "green"?
+                  <AntDesign name="checkcircleo" size={24} color="green" style={{position: "absolute", alignSelf: "flex-end"}}/>
+                  :
+                  <View/>
+                }
+            </View>
+          </View>
         </View>
 
         {/* first name input */}
         <View style={styles.queInput}>
-          <Text style={[material.headline, {color: "black", marginBottom: 10,}]}>First Name:</Text>
-          <TextInput 
-            onChangeText={(value) => {setFirstName(value)}} 
-            style={[material.title, styles.formInput]} 
-            placeholder="Undefined" 
-            value={firstName}
-          >
-          </TextInput>
+          <Text style={[material.title, {color: "grey", marginBottom: 0,}]}>First Name</Text>
+          <View style={{flexDirection: "row"}}>
+            <TextInput 
+              onChangeText={(value) => {setFirstName(value); validateFirstName(value);}} 
+              style={[material.title, styles.formInput,  {borderColor: firstNameValid, flex: 1}]} 
+              placeholder="Enter your firstname" 
+              placeholderTextColor="#aaa"
+              value={firstName}
+            >
+            </TextInput>
+            <View style={{alignItems:"flex-end"}}>
+              {
+                firstNameValid === "green"?
+                <AntDesign name="checkcircleo" size={24} color="green" style={{position: "absolute", alignSelf: "flex-end"}}/>
+                :
+                <View/>
+              }
+              </View>
+          </View>
         </View>
 
         {/* last name input */}
         <View style={styles.queInput}>
-          <Text style={[material.headline, {color: "black", marginBottom: 10,}]}>Last Name:</Text>
-          <TextInput 
-            onChangeText={(value) => {setLastName(value)}} 
-            style={[material.title, styles.formInput]} 
-            placeholder="Undefined" 
-            value={lastName}
-          >
-          </TextInput>
+          <Text style={[material.title, {color: "grey", marginBottom: 0,}]}>Last Name</Text>
+          <View style={{flexDirection: "row"}}>
+            <TextInput 
+              onChangeText={(value) => {setLastName(value); validateLastName(value)}} 
+              style={[material.title, styles.formInput,  {borderColor: lastNameValid, flex: 1}]} 
+              placeholder="Enter your lastname" 
+              placeholderTextColor="#aaa"
+              value={lastName}
+            >
+            </TextInput>
+            <View style={{alignItems:"flex-end"}}>
+              {
+                lastNameValid === "green"?
+                <AntDesign name="checkcircleo" size={24} color="green" style={{position: "absolute", alignSelf: "flex-end"}}/>
+                :
+                <View/>
+              }
+              </View>
+          </View>
         </View>
 
         {/* password input */}
-        <View style={styles.queInput}>
-          <Text style={[material.headline, {color: "black", marginBottom: 10,}]}>password:</Text>
-          <TextInput 
-            onChangeText={(value) => {setPassword(value)}} 
-            style={[material.title, styles.formInput]} 
-            placeholder="Undefined" 
-            value={password}
-          >
-          </TextInput>
+        <View style={[styles.queInput]}>
+          <Text style={[material.title, {color: "grey", marginBottom: 0,}]}>Password</Text>
+          <View style={{flexDirection: "row"}}>
+            <TextInput 
+              onChangeText={(value) => {setPassword(value); validatePassword(value)}} 
+              style={[material.title, styles.formInput,  {borderColor: passwordValid, flex: 1}]} 
+              placeholder="Enter Password" 
+              placeholderTextColor="#aaa"
+              value={password}
+              secureTextEntry={hidePassword}
+            >
+              
+            </TextInput>
+            <Pressable style={{alignItems:"flex-end"}} onPress={() => {setHidePassword(!hidePassword)}}>
+              {
+                hidePassword ?
+                <Ionicons 
+                  name="eye-off" 
+                  size={24} 
+                  color="black" 
+                  style={{position: "absolute", alignSelf: "flex-end"}}
+                />
+                :
+                <AntDesign name="eyeo" size={24} color="black" style={{position: "absolute", alignSelf: "flex-end"}}/>
+              }
+            </Pressable>
+          </View>
         </View>
 
         {/* Date of Birth input using DateTimePickerModal */}
         <View style={styles.dateButtonContainer}>
           <View style={{flexDirection: "row"}}>
-            <Text style={[material.headline, {color: "black"}]}>Date of Birth: </Text>
+            <Text style={[material.headline, {color: "grey"}]}>Date of Birth </Text>
             <TouchableWithoutFeedback onPress={onPressShowDOBDatePicker}>
               <View style={styles.dateButton}>
                 <Text style={material.subheading}>{formatDate(DOB)}</Text>
@@ -302,43 +388,38 @@ const styles = StyleSheet.create(
     headerContainer: {
       width: "100%",
       padding: 5,
-    },
-
-    headerBack: {
-      flexDirection: "row", 
-      marginBottom: 18, 
-      alignContent: "center", 
-      alignItems: "center",
-      backgroundColor: "#64A0EE",
-      width: 100,
-      borderRadius: 200,
-
+      // marginBottom: 50,
     },
 
     formInput: {
-      borderWidth: 1,
-      padding: 10,
+      borderBottomWidth: 1,
+      padding: 8,
       borderRadius: 4,
+
       // color: "red",
+      // backgroundColor: "red",
     },
 
     queInput: {
-      marginBottom: 50,
+      marginBottom: 30,
+      // backgroundColor: "red",
     },
 
     formContainer: {
       flex: 1,
-      padding: 10,
-      // backgroundColor: "red",
+      padding: 25,
+      backgroundColor: "white",
       // marginBottom: 100,
     },
 
     submitButton: {
-      alignSelf: "center",
+      // alignSelf: "center",
       borderWidth: 1,
       padding: 10,
-      borderRadius: 30,
-      backgroundColor: "#569AF3"
+      borderRadius: 10,
+      backgroundColor: "#569AF3",
+      flex: 1,
+      alignItems: "center",
     },
 
     dateButton: {
@@ -364,9 +445,9 @@ const styles = StyleSheet.create(
 
     avatar: {
       overflow: "hidden",
-      width: 200,
-      height: 200,
-      borderRadius: 100,
+      width: 120,
+      height: 120,
+      borderRadius: 60,
       alignSelf: "center",
     },
   }
