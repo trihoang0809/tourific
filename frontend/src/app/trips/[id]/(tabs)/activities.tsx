361b7ae5a6562eb935f6de369504302b55ea29e3
Tri Hoang <trihoang0809@gmail.com>
2024-05-13 16:49:55 +0700

    implement filter + search for activities

--- frontend/src/app/trips/[id]/(tabs)/activities.tsx
+++ frontend/src/app/trips/[id]/(tabs)/activities.tsx
@@ -1,11 +1,114 @@
-import { View, ScrollView, TouchableOpacity, TextInput } from "react-native";
+import {
+  View,
+  ScrollView,
+  TouchableOpacity,
+  TextInput,
+  Text,
+} from "react-native";
 import { useState, useEffect } from "react";
 import { useGlobalSearchParams } from "expo-router";
 import { Ionicons, Feather } from "@expo/vector-icons";
-import { Dimensions } from "react-native";
+import { Dimensions, StyleSheet } from "react-native";
 import ActivityThumbnail from "@/components/ActivityThumbnail";
 import { ActivityProps } from "@/types";
 import { fetchActivities } from "@/utils/fetchActivities";
+
+type CategoryKeys =
+  | "Dining"
+  | "Entertainment"
+  | "OutdoorRecreation"
+  | "Shopping"
+  | "Services"
+  | "Transportation"
+  | "Wellness";
+
+const categories: Record<CategoryKeys, string[]> = {
+  Dining: [
+    "restaurant",
+    "cafe",
+    "bakery",
+    "bar",
+    "meal_delivery",
+    "meal_takeaway",
+    "food",
+  ],
+  Entertainment: [
+    "movie_theater",
+    "night_club",
+    "amusement_park",
+    "museum",
+    "library",
+    "art_gallery",
+    "bar",
+    "tourist_attraction",
+    "casino",
+    "bowling_alley",
+  ],
+  OutdoorRecreation: [
+    "park",
+    "zoo",
+    "campground",
+    "aquarium",
+    "university",
+    "stadium",
+    "city_hall",
+    "church",
+  ],
+  Shopping: [
+    "clothing_store",
+    "shopping_mall",
+    "book_store",
+    "jewelry_store",
+    "liquor_store",
+    "home_goods_store",
+    "store",
+    "furniture_store",
+    "supermarket",
+    "pet_store",
+    "florist",
+    "convenience_store",
+    "movie_rental",
+    "hardware_store",
+    "",
+  ],
+  Services: [
+    "car_rental",
+    "car_repair",
+    "laundry",
+    "bank",
+    "accounting",
+    "lawyer",
+    "atm",
+    "car_dealer",
+    "plumber",
+    "police",
+    "post_office",
+    "electrician",
+    "electronics_store",
+    "embassy",
+    "fire_station",
+    "storage",
+  ],
+  Transportation: [
+    "airport",
+    "transit_station",
+    "train_station",
+    "subway_station",
+    "bus_station",
+  ],
+  Wellness: [
+    "gym",
+    "hair_care",
+    "hospital",
+    "spa",
+    "doctor",
+    "drugstore",
+    "dentist",
+    "pharmacy",
+    "physiotherapist",
+    "beauty_salon",
+  ],
+};
 
 const ActivitiesScreen = () => {
   const { id } = useGlobalSearchParams();
@@ -23,10 +126,14 @@ const ActivitiesScreen = () => {
     startHour: 0,
     startMinute: 0,
   });
+  const [activities, setActivities] = useState<ActivityProps[]>([]);
+  const [filteredActivities, setFilteredActivities] = useState<ActivityProps[]>(
+    [],
+  );
   const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
-    const getTrip = async ({ id: text }: { id: string }) => {
+    const getTripAndActivities = async () => {
       try {
         const response = await fetch(`http://localhost:3000/trips/${id}`, {
           method: "GET",
@@ -39,59 +146,86 @@ const ActivitiesScreen = () => {
         }
         const data = await response.json();
         setTrip(data);
+        if (
+          data.location.latitude &&
+          data.location.longitude &&
+          data.location.radius
+        ) {
+          const fetchedActivities = await fetchActivities(
+            data.location.latitude,
+            data.location.longitude,
+            data.location.radius,
+          );
+          setActivities(fetchedActivities);
+          setFilteredActivities(fetchedActivities);
+          //await saveActivitiesToBackend(fetchedActivities);
+        }
       } catch (error: any) {
         console.error("Error fetching trip:", error.toString());
       }
     };
-    getTrip({ id });
-  }, []);
-
-  const activities = fetchActivities(
-    trip.location.latitude,
-    trip.location.longitude,
-    trip.location.radius,
-  );
+    getTripAndActivities();
+  }, [id]);
 
   const saveActivitiesToBackend = async (activities: ActivityProps[]) => {
-    console.log("start saving");
-    activities.forEach(async (activity) => {
-      try {
-        const response = await fetch(
-          `http://localhost:3000/trips/${id}/activities`,
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              name: activity.name,
-              description: activity.description,
-              startTime: new Date(),
-              endTime: new Date(),
-              location: {
-                citystate: activity.location.citystate,
-                latitude: activity.location.latitude,
-                longitude: activity.location.longitude,
-              },
-              notes: activity.notes,
-              netUpvotes: activity.netUpvotes,
-              isOnCalendar: activity.isOnCalendar,
-              category: activity.category,
-              rating: activity.rating,
-            }),
+    const promises = activities.map(async (activity) => {
+      const response = await fetch(
+        `http://localhost:3000/trips/${id}/activities`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
           },
-        );
-      } catch (error) {
-        console.log(error);
-      }
+          body: JSON.stringify({
+            name: activity.name,
+            description: activity.description,
+            startTime: new Date(),
+            endTime: new Date(),
+            location: {
+              citystate: activity.location.citystate,
+              latitude: activity.location.latitude,
+              longitude: activity.location.longitude,
+            },
+            notes: activity.notes,
+            netUpvotes: activity.netUpvotes,
+            isOnCalendar: activity.isOnCalendar,
+            category: activity.category,
+            rating: activity.rating,
+          }),
+        },
+      );
+      return response.json();
     });
+    try {
+      //const newActivities = await Promise.all(promises);
+      //setActivities(newActivities); // Update the activities state with new data including IDs
+    } catch (error) {
+      console.error("Error saving activities:", error);
+    }
   };
 
-  useEffect(() => {
-    saveActivitiesToBackend(activities);
-  }, [[activities]]);
+  const handleSelectCategory = (category: CategoryKeys | "All") => {
+    if (category === "All") {
+      setFilteredActivities(activities);
+    } else {
+      const filtered = activities.filter((activity) =>
+        activity.category.some((type) => categories[category].includes(type)),
+      );
+      setFilteredActivities(filtered);
+    }
+  };
 
-  const handleSubmit = () => {}; // to be implemented for filtering
+  const handleSearch = (text: string) => {
+    setSearchTerm(text);
+    if (text) {
+      const searchedActivities = activities.filter((activity) =>
+        activity.name.toLowerCase().includes(text.toLowerCase()),
+      );
+      setFilteredActivities(searchedActivities);
+    } else {
+      setFilteredActivities(activities);
+    }
+  };
 
   return (
     <View
@@ -101,15 +235,6 @@ const ActivitiesScreen = () => {
         backgroundColor: "white",
       }}
     >
-      {/* <Stack.Screen
-        options={{
-          title: '',
-          headerShown: true,
-          headerBackTitle: 'Back',
-          headerBackTitleStyle: { fontSize: 10 },
-          
-        }}
-      /> */}
       <View
         style={{
           flexDirection: "row",
@@ -125,8 +250,7 @@ const ActivitiesScreen = () => {
         <TextInput
           placeholder="Search activities..."
           value={searchTerm}
-          onChangeText={setSearchTerm}
-          onSubmitEditing={handleSubmit}
+          onChangeText={handleSearch}
           style={{
             padding: 5,
             flex: 1,
@@ -136,6 +260,64 @@ const ActivitiesScreen = () => {
           }}
         />
       </View>
+      <ScrollView horizontal contentContainerStyle={styles.categoryContainer}>
+        <TouchableOpacity
+          style={styles.categoryItem}
+          onPress={() => handleSelectCategory("All")}
+        >
+          <Ionicons name="apps-outline" size={24} color="black" />
+          <Text>All</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.categoryItem}
+          onPress={() => handleSelectCategory("Dining")}
+        >
+          <Ionicons name="restaurant-outline" size={24} color="black" />
+          <Text>Dining</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.categoryItem}
+          onPress={() => handleSelectCategory("Entertainment")}
+        >
+          <Ionicons name="film-outline" size={24} color="black" />
+          <Text>Entertainment</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.categoryItem}
+          onPress={() => handleSelectCategory("OutdoorRecreation")}
+        >
+          <Ionicons name="partly-sunny-outline" size={24} color="black" />
+          <Text>Outdoor</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.categoryItem}
+          onPress={() => handleSelectCategory("Shopping")}
+        >
+          <Feather name="shopping-cart" size={24} color="black" />
+          <Text>Shopping</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.categoryItem}
+          onPress={() => handleSelectCategory("Transportation")}
+        >
+          <Ionicons name="bus-outline" size={24} color="black" />
+          <Text>Transportation</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.categoryItem}
+          onPress={() => handleSelectCategory("Services")}
+        >
+          <Ionicons name="settings-outline" size={24} color="black" />
+          <Text>Services</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.categoryItem}
+          onPress={() => handleSelectCategory("Wellness")}
+        >
+          <Feather name="activity" size={24} color="black" />
+          <Text>Wellness</Text>
+        </TouchableOpacity>
+      </ScrollView>
       <ScrollView
         contentContainerStyle={{
           flexGrow: 1,
@@ -144,15 +326,30 @@ const ActivitiesScreen = () => {
           padding: 5,
         }}
       >
-        {/* <TouchableOpacity style={{ backgroundColor: '#fff', borderRadius: 10, alignItems: 'center', justifyContent: 'center', width: '50%',height: 250, padding: 10}} 
-          onPress={() => { }}>
-          <Text>Add</Text>
-        </TouchableOpacity> */}
-        {activities.map((activity: ActivityProps, index: number) => (
-          <View style={{ width: "100%", padding: 15 }}>
-            <ActivityThumbnail key={index} {...activity} />
+        {filteredActivities.length > 0 ? (
+          <ScrollView
+            contentContainerStyle={{
+              flexGrow: 1,
+              flexDirection: "row",
+              flexWrap: "wrap",
+              padding: 5,
+            }}
+          >
+            {filteredActivities.map(
+              (activity: ActivityProps, index: number) => (
+                <View key={index} style={{ width: "100%", padding: 15 }}>
+                  <ActivityThumbnail activity={activity} tripId={id} />
+                </View>
+              ),
+            )}
+          </ScrollView>
+        ) : (
+          <View style={{ flex: 1, alignItems: "center" }}>
+            <Text style={{ fontSize: 18, color: "#666" }}>
+              No activities found for this category.
+            </Text>
           </View>
-        ))}
+        )}
       </ScrollView>
       <TouchableOpacity
         style={{
@@ -179,5 +376,33 @@ const ActivitiesScreen = () => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  searchContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#E6E6E6",
+    borderRadius: 20,
+    borderWidth: 1,
+    padding: 10,
+    margin: 10,
+  },
+  searchInput: {
+    padding: 5,
+    flex: 1,
+    height: 30,
+    fontSize: 16,
+    color: "black",
+  },
+  categoryContainer: {
+    justifyContent: "space-between",
+    paddingVertical: 3,
+    paddingHorizontal: 3,
+  },
+  categoryItem: {
+    alignItems: "center",
+    padding: 15,
+  },
+});
 
 export default ActivitiesScreen;
--- frontend/src/utils/fetchActivities.tsx
+++ frontend/src/utils/fetchActivities.tsx
@@ -1,16 +1,14 @@
-import { useState, useEffect } from "react";
 import { ActivityProps } from "@/types";
 
-export const fetchActivities = (
+export const fetchActivities = async (
   latitude: number,
   longitude: number,
   radius: number,
-): ActivityProps[] => {
-  const [activities, setActivities] = useState<ActivityProps[]>([]);
+) => {
   const GOOGLE_PLACES_API_KEY =
     process.env.EXPO_PUBLIC_GOOGLE_PLACES_API_KEY || "undefined";
 
-  const getImageUrl = (photos: any, apiKey: any) => {
+  const getImageUrl = (photos: any, apiKey: string) => {
     if (photos && photos.length > 0) {
       const photoReference = photos[0].photo_reference;
       return `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=${photoReference}&key=${apiKey}`;
@@ -18,40 +16,29 @@ export const fetchActivities = (
     return "https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0";
   };
 
-  useEffect(() => {
-    const doFetch = async () => {
-      try {
-        const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude}%2C${longitude}&radius=${radius}&key=${GOOGLE_PLACES_API_KEY}`;
-        const response = await fetch(url);
-        const data = await response.json();
+  try {
+    const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude}%2C${longitude}&radius=${radius}&key=${GOOGLE_PLACES_API_KEY}`;
+    const response = await fetch(url);
+    const data = await response.json();
 
-        const activitiesData: ActivityProps[] = data.results.map(
-          (place: any) => ({
-            id: place.place_id,
-            name: place.name,
-            description: "",
-            imageUrl: getImageUrl(place.photos, GOOGLE_PLACES_API_KEY),
-            location: {
-              citystate: place.vicinity,
-              latitude: place.geometry.location.lat,
-              longitude: place.geometry.location.lng,
-            },
-            notes: "",
-            netUpvotes: 0,
-            isOnCalendar: false,
-            category: place.types,
-            rating: place.rating,
-          }),
-        );
-
-        setActivities(activitiesData);
-      } catch (error) {
-        console.error("Error fetching activities:", error);
-      }
-    };
-
-    doFetch();
-  }, [latitude, longitude, radius]);
-
-  return activities;
+    const activitiesData: ActivityProps[] = data.results.map((place: any) => ({
+      name: place.name,
+      description: "",
+      imageUrl: getImageUrl(place.photos, GOOGLE_PLACES_API_KEY),
+      location: {
+        citystate: place.vicinity,
+        latitude: place.geometry.location.lat,
+        longitude: place.geometry.location.lng,
+      },
+      notes: "",
+      netUpvotes: 0,
+      isOnCalendar: false,
+      category: place.types,
+      rating: place.rating,
+    }));
+    return activitiesData;
+  } catch (error) {
+    console.error("Error fetching activities:", error);
+    return [];
+  }
 };
