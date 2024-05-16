05c72208fb97d9f220858766c701980d9c99267e
Van Nguyen <87792873+yourvivian@users.noreply.github.com>
2024-05-16 07:44:00 -0700

    Calendar navigation

--- frontend/src/app/trips/[id]/(tabs)/(itinerary)/index.tsx
+++ frontend/src/app/trips/[id]/(tabs)/(itinerary)/index.tsx
@@ -1,85 +1,56 @@
-import React, { Component, useState, useEffect } from "react";
-import { View, Text } from "react-native";
+import React, { useState, useEffect, useCallback } from "react";
+import { View, Text, TouchableOpacity } from "react-native";
 // import testIDs from '../testIDs';
-import { Link, Stack, useGlobalSearchParams } from "expo-router";
-import { Calendar } from "react-native-big-calendar";
-import { mockData } from "@/mock-data/activities";
+import { useGlobalSearchParams, router } from "expo-router";
+import { Calendar, DateRangeHandler } from "react-native-big-calendar";
 import DropDown from "@/components/DropDown";
 import { Mode } from "react-native-big-calendar/build/interfaces";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { StyleSheet } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
 
-const eventNotes = (
-  <View style={{ marginTop: 3 }}>
-    <Text style={{ fontSize: 10, color: "white" }}>
-      {" "}
-      Phone number: 555-123-4567{" "}
-    </Text>
-    <Text style={{ fontSize: 10, color: "white" }}>
-      {" "}
-      Arrive 15 minutes early{" "}
-    </Text>
-  </View>
-);
+interface Activity {
+  isOnCalendar: boolean;
+  name: string;
+  startTime: Date;
+  endTime: Date;
+  location: {
+    address: string;
+    citystate: string;
+  };
+  id: string;
+}
 
-const events = mockData
-  ? mockData
-      .filter((activity) => activity.isOnCalendar)
-      .map((activity) => {
-        return {
-          title: activity.name,
-          start: activity.startTime,
-          end: activity.endTime,
-          children: eventNotes,
-        };
-      })
-  : [];
-
-// const events = [
-//   {
-//     title: 'Meeting',
-//     start: new Date(2024, 4, 7, 10, 0),
-//     end: new Date(2024, 4, 7, 10, 30),
-//   },
-//   {
-//     title: 'Coffee break',
-//     start: new Date(2024, 4, 8, 15, 45),
-//     end: new Date(2024, 4, 8, 16, 30),
-//   },
-//   {
-//     title: 'Repair my car',
-//     start: new Date(2024, 4, 20, 7, 45),
-//     end: new Date(2024, 4, 20, 13, 30),
-//   },
-// ]
+interface Event {
+  title: string;
+  start: Date;
+  end: Date;
+  children: JSX.Element;
+  activityid: string;
+}
 
 const Itinerary = () => {
   const { id } = useGlobalSearchParams();
-  console.log("id (initerary page):", id);
+  // console.log("id (initerary page):", id);
+  const [activities, setActivities] = useState<Activity[] | []>([]);
+  const [events, setEvents] = useState<Event[]>([]);
 
-  const [activities, setActivities] = useState(null);
-
-  const getActivities = async ({
-    id,
-  }: {
-    id: string;
-  }) => {
+  const getActivities = async ({ id }: { id: string }) => {
     try {
-      const response = await fetch(
-        `http://localhost:3000/trips/${id}/activities/`,
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          // body: JSON.stringify(req),
+      const response = await fetch(`http://localhost:3000/trips/${id}/`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
         },
-      );
+      });
       if (!response.ok) {
         throw new Error("Failed to fetch activities");
       }
       const data = await response.json();
-      setActivities(data);
-      console.log("Activities fetch:", data);
+      const activities = data.activities;
+      setActivities(activities);
+      // console.log("First activities fetch:", activities[0]);
+      // console.log("First events fetch:", events[0]);
     } catch (error: any) {
       console.error("Error fetching activities:", error.toString());
     }
@@ -89,11 +60,90 @@ const Itinerary = () => {
     getActivities({ id });
   }, []);
 
+  useEffect(() => {
+    const filteredEvents = activities
+      .filter((activity) => activity.isOnCalendar)
+      .map((activity) => {
+        const eventNotes = activity.location ? (
+          <View style={{ marginTop: 3 }}>
+            <Text style={{ fontSize: 10, color: "white" }}>
+              {activity.location.address}
+            </Text>
+            <Text style={{ fontSize: 10, color: "white" }}>
+              {activity.location.citystate}
+            </Text>
+          </View>
+        ) : null;
 
+        return {
+          title: activity.name,
+          start: new Date(activity.startTime),
+          end: new Date(activity.endTime),
+          children: eventNotes,
+          activityid: activity.id,
+        };
+      });
+    setEvents(filteredEvents);
+    //  console.log("events here:", filteredEvents);
+  }, [activities]);
 
-  const [calendarMode, setCalendarMode] = useState<Mode>("3days");
-  console.log("logged by start: ", new Date(2020, 5, 20, 10, 0));
-  console.log("logged by end: ", new Date(2020, 5, 20, 22, 30));
+  // events && console.log("events here:", events);
+
+  const [calendarMode, setCalendarMode] = useState<Mode>("schedule");
+  const [currentDate, setCurrentDate] = useState(new Date(Date.now()));
+  const handleNextWeek = () => {
+    const nextWeek = new Date(currentDate.getTime() + 7 * 24 * 60 * 60 * 1000);
+    setCurrentDate(nextWeek);
+  };
+  const handlePrevWeek = () => {
+    const prevWeek = new Date(currentDate.getTime() - 7 * 24 * 60 * 60 * 1000);
+    setCurrentDate(prevWeek);
+  };
+  const handleNextDay = () => {
+    const nextDay = new Date(currentDate.getTime() + 24 * 60 * 60 * 1000);
+    setCurrentDate(nextDay);
+  };
+  const handlePrevDay = () => {
+    const prevDay = new Date(currentDate.getTime() - 24 * 60 * 60 * 1000);
+    setCurrentDate(prevDay);
+  };
+  const handlePrev3Days = () => {
+    const prevThreeDays = new Date(
+      currentDate.getTime() - 3 * 24 * 60 * 60 * 1000,
+    );
+    setCurrentDate(prevThreeDays);
+  };
+  const handleNext3Days = () => {
+    const nextThreeDays = new Date(
+      currentDate.getTime() + 3 * 24 * 60 * 60 * 1000,
+    );
+    setCurrentDate(nextThreeDays);
+  };
+  const handleNextMonth = () => {
+    const nextMonth = new Date(
+      currentDate.getFullYear(),
+      currentDate.getMonth() + 1,
+      currentDate.getDate(),
+    );
+    setCurrentDate(nextMonth);
+  };
+  const handlePrevMonth = () => {
+    const prevMonth = new Date(
+      currentDate.getFullYear(),
+      currentDate.getMonth() - 1,
+      currentDate.getDate(),
+    );
+    setCurrentDate(prevMonth);
+  };
+
+  const handleChangeDate: DateRangeHandler = useCallback(
+    ([start, end]: [Date, Date]) => {
+      setCurrentDate(start);
+      console.log("Current date updated:", start.toISOString());
+    },
+    [],
+  );
+
   const labels = [
     { label: "Schedule", value: "schedule" },
     { label: "Day", value: "day" },
@@ -101,12 +151,76 @@ const Itinerary = () => {
     { label: "Week", value: "week" },
     { label: "Month", value: "month" },
   ];
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
 
   return (
-    <View style={{ flex: 1 }}>
-      <Link href={`trips/${id}/(itinerary)/663ed3689464d811386b288f`}>
-        Click here
-      </Link>
+    <View style={{ flex: 1, backgroundColor: "white" }}>
+      <View style={styles.row}>
+        <TouchableOpacity
+          onPress={() => {
+            switch (calendarMode) {
+              case "week":
+                handlePrevWeek();
+                break;
+              case "day":
+                handlePrevDay();
+                break;
+              case "3days":
+                handlePrev3Days();
+                break;
+              case "month":
+                handlePrevMonth();
+                break;
+              default:
+                break;
+            }
+          }}
+          // style={styles.btnPrevNext}
+        >
+          <View style={styles.leftRightIcon}>
+            <AntDesign name="left" size={24} color="black" />
+          </View>
+        </TouchableOpacity>
+        <Text style={[styles.h1, {marginHorizontal: 10}]}>{monthNames[currentDate.getMonth()]}</Text>
+        <TouchableOpacity
+          onPress={() => {
+            switch (calendarMode) {
+              case "week":
+                handleNextWeek();
+                break;
+              case "day":
+                handleNextDay();
+                break;
+              case "3days":
+                handleNext3Days();
+                break;
+              case "month":
+                handleNextMonth();
+                break;
+              default:
+                break;
+            }
+          }}
+          // style={styles.btnPrevNext}
+        >
+          <View style={styles.leftRightIcon}>
+            <AntDesign name="right" size={24} color="black" />
+          </View>
+        </TouchableOpacity>
+      </View>
       <DropDown
         labels={labels}
         onChange={(mode: Mode) => setCalendarMode(mode)}
@@ -116,23 +230,37 @@ const Itinerary = () => {
         }
       />
       <Calendar
-        // height={Dimensions.get('window').height}
-        // style={styles.calendar}
         events={events}
         height={600}
+        onChangeDate={handleChangeDate}
         mode={calendarMode}
         onPressEvent={(event) => {
-          return (
-            <Link
-              href={`trips/${id}/activities/663ed3689464d811386b288f`}
-            ></Link>
-          );
+          router.push(`trips/${id}/(itinerary)/${event.activityid}`);
         }} // eventCellStyle={{ backgroundColor: 'blue' }}
+        date={currentDate}
+        swipeEnabled={false}
       />
     </View>
   );
 };
-
+const styles = StyleSheet.create({
+  h1: {
+    fontWeight: "600",
+    fontSize: 26,
+  },
+  row: {
+    flexDirection: "row",
+    marginTop: 18,
+    alignItems: "center",
+    alignContent: "center",
+  },
+  leftRightIcon: {
+    borderWidth: 1,
+    borderColor: "#006ee6",
+    borderRadius: 50,
+    padding: 4,
+  },
+});
 export default Itinerary;
 
 // export default class Itinerary extends React.Component {
