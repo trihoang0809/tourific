5f0143ef6ae03400f72ca44ca2ea02c3e3952b51
Van Nguyen <87792873+yourvivian@users.noreply.github.com>
2024-05-16 10:27:03 -0700

    Restyle calendar

--- frontend/src/app/trips/[id]/(tabs)/(itinerary)/index.tsx
+++ frontend/src/app/trips/[id]/(tabs)/(itinerary)/index.tsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect, useCallback } from "react";
-import { View, Text, TouchableOpacity } from "react-native";
-// import testIDs from '../testIDs';
+import { View, Text, TouchableOpacity, Modal, Pressable } from "react-native";
 import { useGlobalSearchParams, router } from "expo-router";
 import { Calendar, DateRangeHandler } from "react-native-big-calendar";
-import DropDown from "@/components/DropDown";
 import { Mode } from "react-native-big-calendar/build/interfaces";
-import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import { StyleSheet } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Entypo from "@expo/vector-icons/Entypo";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 interface Activity {
   isOnCalendar: boolean;
@@ -31,7 +31,6 @@ interface Event {
 
 const Itinerary = () => {
   const { id } = useGlobalSearchParams();
-  // console.log("id (initerary page):", id);
   const [activities, setActivities] = useState<Activity[] | []>([]);
   const [events, setEvents] = useState<Event[]>([]);
 
@@ -84,10 +83,7 @@ const Itinerary = () => {
         };
       });
     setEvents(filteredEvents);
-    //  console.log("events here:", filteredEvents);
   }, [activities]);
-
-  // events && console.log("events here:", events);
 
   const [calendarMode, setCalendarMode] = useState<Mode>("schedule");
   const [currentDate, setCurrentDate] = useState(new Date(Date.now()));
@@ -135,15 +131,13 @@ const Itinerary = () => {
     );
     setCurrentDate(prevMonth);
   };
-
   const handleChangeDate: DateRangeHandler = useCallback(
     ([start, end]: [Date, Date]) => {
       setCurrentDate(start);
-      console.log("Current date updated:", start.toISOString());
+      // console.log("Current date updated:", start.toISOString());
     },
     [],
   );
-
   const labels = [
     { label: "Schedule", value: "schedule" },
     { label: "Day", value: "day" },
@@ -165,70 +159,149 @@ const Itinerary = () => {
     "November",
     "December",
   ];
+  const [isModalVisible, setModalVisible] = useState(false);
+  const customTheme = {
+    palette: {
+      primary: {
+        main: "#006ee6",
+        contrastText: "#fff",
+      },
+    },
+    typography: {
+      xs: {
+        fontWeight: "400",
+        fontSize: 12,
+        lineHeight: 16,
+        letterSpacing: 0.5,
+      },
+      sm: {
+        fontWeight: "400",
+        fontSize: 14,
+        lineHeight: 20,
+        letterSpacing: 0.25,
+      },
+      xl: {
+        fontWeight: "500",
+        fontSize: 24,
+        lineHeight: 28,
+        letterSpacing: 0.15,
+      },
+    },
+    eventCellOverlappings: [
+      {
+        main: "#e6352b", // Tomato color
+        contrastText: "#fff",
+      },
+    ],
+  };
 
   return (
     <View style={{ flex: 1, backgroundColor: "white" }}>
-      <View style={styles.row}>
-        <TouchableOpacity
-          onPress={() => {
-            switch (calendarMode) {
-              case "week":
-                handlePrevWeek();
-                break;
-              case "day":
-                handlePrevDay();
-                break;
-              case "3days":
-                handlePrev3Days();
-                break;
-              case "month":
-                handlePrevMonth();
-                break;
-              default:
-                break;
-            }
-          }}
-          // style={styles.btnPrevNext}
-        >
-          <View style={styles.leftRightIcon}>
-            <AntDesign name="left" size={24} color="black" />
-          </View>
-        </TouchableOpacity>
-        <Text style={[styles.h1, {marginHorizontal: 10}]}>{monthNames[currentDate.getMonth()]}</Text>
-        <TouchableOpacity
-          onPress={() => {
-            switch (calendarMode) {
-              case "week":
-                handleNextWeek();
-                break;
-              case "day":
-                handleNextDay();
-                break;
-              case "3days":
-                handleNext3Days();
-                break;
-              case "month":
-                handleNextMonth();
-                break;
-              default:
-                break;
-            }
-          }}
-          // style={styles.btnPrevNext}
-        >
-          <View style={styles.leftRightIcon}>
-            <AntDesign name="right" size={24} color="black" />
-          </View>
-        </TouchableOpacity>
+      <View
+        style={[
+          styles.row,
+          calendarMode === "schedule"
+            ? { justifyContent: "center" }
+            : { justifyContent: "space-between" },
+        ]}
+      >
+        {calendarMode !== "schedule" && (
+          <TouchableOpacity
+            onPress={() => {
+              switch (calendarMode) {
+                case "week":
+                  handlePrevWeek();
+                  break;
+                case "day":
+                  handlePrevDay();
+                  break;
+                case "3days":
+                  handlePrev3Days();
+                  break;
+                case "month":
+                  handlePrevMonth();
+                  break;
+                default:
+                  break;
+              }
+            }}
+            // style={styles.btnPrevNext}
+          >
+            <View style={styles.leftRightIcon}>
+              <AntDesign name="left" size={20} color="black" />
+            </View>
+          </TouchableOpacity>
+        )}
+        <View style={styles.monthContainer}>
+          <Text style={styles.h1}>{monthNames[currentDate.getMonth()]}</Text>
+          <Pressable onPress={() => setModalVisible(true)}>
+            <Entypo name="chevron-down" size={24} color="black" />
+          </Pressable>
+        </View>
+        {calendarMode !== "schedule" && (
+          <TouchableOpacity
+            onPress={() => {
+              switch (calendarMode) {
+                case "week":
+                  handleNextWeek();
+                  break;
+                case "day":
+                  handleNextDay();
+                  break;
+                case "3days":
+                  handleNext3Days();
+                  break;
+                case "month":
+                  handleNextMonth();
+                  break;
+                default:
+                  break;
+              }
+            }}
+          >
+            <View style={styles.leftRightIcon}>
+              <AntDesign name="right" size={20} color="black" />
+            </View>
+          </TouchableOpacity>
+        )}
       </View>
-      <DropDown
-        labels={labels}
-        onChange={(mode: Mode) => setCalendarMode(mode)}
-        name="Calendar View"
-        icon={
-          <MaterialIcons name="calendar-view-week" size={24} color="black" />
-        }
-      />
+      <Modal animationType="slide" visible={isModalVisible} transparent={true}>
+        <View style={{ alignItems: "center", top: "20%" }}>
+          <View style={styles.modalView}>
+            <SafeAreaView style={styles.calendarViewContainer}>
+              {labels.map((view, index) => (
+                <Pressable
+                  key={index}
+                  onPress={() => {
+                    setCalendarMode(view.value as Mode);
+                    setModalVisible(!isModalVisible);
+                  }}
+                  style={
+                    calendarMode === view.value
+                      ? styles.calendarViewSelectedOption
+                      : styles.calendarViewOption
+                  }
+                >
+                  <Text
+                    style={
+                      calendarMode === view.value ? { color: "white" } : {}
+                    }
+                  >
+                    {view.label}
+                  </Text>
+                </Pressable>
+              ))}
+            </SafeAreaView>
+            <Pressable
+              style={styles.cancelButton}
+              onPress={() => setModalVisible(!isModalVisible)}
+            >
+              <Ionicons name="close" size={24} color="black" />
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+
       <Calendar
         events={events}
         height={600}
@@ -239,6 +312,7 @@ const Itinerary = () => {
         }} // eventCellStyle={{ backgroundColor: 'blue' }}
         date={currentDate}
         swipeEnabled={false}
+        theme={customTheme}
       />
     </View>
   );
@@ -249,16 +323,57 @@ const styles = StyleSheet.create({
     fontSize: 26,
   },
   row: {
+    display: "flex",
     flexDirection: "row",
-    marginTop: 18,
     alignItems: "center",
-    alignContent: "center",
+    justifyContent: "space-between",
+    padding: 15,
   },
   leftRightIcon: {
     borderWidth: 1,
     borderColor: "#006ee6",
     borderRadius: 50,
     padding: 4,
+  },
+  monthContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  modalView: {
+    width: "80%",
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 20,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  calendarViewContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
+  calendarViewOption: {
+    backgroundColor: "#ddd",
+    padding: 10,
+    margin: 5,
+    borderRadius: 5,
+  },
+  calendarViewSelectedOption: {
+    backgroundColor: "#006ee6",
+    padding: 10,
+    margin: 5,
+    borderRadius: 5,
+    color: "white",
+  },
+  cancelButton: {
+    position: "absolute",
+    top: 15,
+    right: 15,
   },
 });
 export default Itinerary;
