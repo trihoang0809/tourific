0a4b10b2e5f3ed1de08396878449005bd9a4640e
Van Nguyen <87792873+yourvivian@users.noreply.github.com>
2024-05-16 10:25:25 -0700

    Delete navigation

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -31,9 +31,6 @@ export default function App() {
         Go to trip at the beach
       </Link>
       <Link href="/trips/create">Go to trip create</Link>
-      <Link href="trips/661f78b88c72a65f2f6e49d4/(activities)/663ed3689464d811386b288f">
-        Go to an activity
-      </Link>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
