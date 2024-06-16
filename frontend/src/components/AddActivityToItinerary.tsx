bb868cac5a9741b7f8d928a96f8df7f44d4361e9
Van Nguyen <87792873+yourvivian@users.noreply.github.com>
2024-06-15 17:38:11 -0700

    Style AddActivity page

--- frontend/src/components/AddActivityToItinerary.tsx
+++ frontend/src/components/AddActivityToItinerary.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { SearchBar } from "@rneui/themed";
 import {
   View,
@@ -9,23 +9,10 @@ import {
   Modal,
   SafeAreaView,
   Pressable,
+  Image,
 } from "react-native";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { ActivityProps, AddActivityProps } from "@/types";
-
-interface Result {
-  id: string;
-  urls: {
-    small: string;
-  };
-  user: {
-    name: string;
-    links: {
-      html: string;
-    };
-  };
-}
-
 
 const AddActivityToItinerary = ({
   currentDateUpdate,
@@ -34,20 +21,29 @@ const AddActivityToItinerary = ({
   isVisible,
   setIsVisible,
 }: AddActivityProps) => {
-  // May need the following for search bar
-  const [query, setQuery] = useState("scenery");
-  const [results, setResults] = useState<Result[]>([]);
   const updateQuery = (query: string) => {
-    setQuery(query);
     setSearchBarValue(query);
   };
   const [searchBarValue, setSearchBarValue] = useState("");
+  const [filteredActivities, setFilteredActivities] = useState<ActivityProps[]>(
+    [],
+  );
+
+  useEffect(() => {
+    // Filter activities based on query whenever input or query changes
+    const filtered = input.filter((activity) =>
+      activity.name.toLowerCase().includes(searchBarValue.toLowerCase()),
+    );
+    setFilteredActivities(filtered);
+  }, [input, searchBarValue]);
 
   return (
     <Modal animationType="slide" visible={isVisible}>
-      <SafeAreaView style={{ flex: 1, backgroundColor: "transparent" }}>
+      <SafeAreaView style={{ flex: 1, height: "100%" }}>
         <View style={styles.row}>
-          <Text style={styles.text}>{currentDateUpdate.toDateString()}</Text>
+          <Text style={[styles.modalText, { paddingLeft: 15 }]}>
+            What to do on {currentDateUpdate.toLocaleDateString()}?
+          </Text>
           <Pressable
             style={styles.closeButton}
             onPress={() => setIsVisible(false)}
@@ -55,27 +51,40 @@ const AddActivityToItinerary = ({
             <Ionicons name="close" size={24} color="black" />
           </Pressable>
         </View>
-        <SearchBar
-          placeholder="Search a place..."
-          onChangeText={updateQuery}
-          value={searchBarValue}
-          lightTheme={true}
-          round={true}
-          containerStyle={{
-            backgroundColor: "white",
-            borderTopColor: "white",
-            borderBottomColor: "white",
-            padding: 0,
-          }}
-        />
-        <Text style={styles.note}>Or select from existing activities</Text>
-        <ScrollView>
-          <View style={styles.imageContainer}>
-            {input &&
-              input.map((activity: ActivityProps) => {
-                return (
-                  <View>
+        <View
+          style={{ backgroundColor: "#e9e9e9", padding: 20, marginBottom: 100 }}
+        >
+          <SearchBar
+            placeholder="Search from upvoted activities..."
+            onChangeText={updateQuery}
+            value={searchBarValue}
+            lightTheme={true}
+            round={true}
+            containerStyle={{
+              backgroundColor: "#e9e9e9",
+              justifyContent: "space-around",
+              borderTopColor: "#e9e9e9",
+              borderBottomColor: "#e9e9e9",
+              padding: 0,
+              paddingBottom: 20,
+            }}
+            inputContainerStyle={{ backgroundColor: "white" }}
+            inputStyle={{ backgroundColor: "white", fontSize: 16 }}
+            leftIconContainerStyle={{ backgroundColor: "white" }}
+          />
+          <ScrollView>
+            <View>
+              {filteredActivities.length > 0 ? (
+                filteredActivities.map((activity: ActivityProps) => {
+                  return (
                     <TouchableOpacity
+                      style={{
+                        backgroundColor: "white",
+                        paddingHorizontal: 5,
+                        width: "100%",
+                        borderRadius: 8,
+                        marginBottom: 15,
+                      }}
                       onPress={() => {
                         console.log(
                           "current date in add activity modal, ",
@@ -85,15 +94,66 @@ const AddActivityToItinerary = ({
                         setIsVisible(false);
                       }}
                     >
-                      <Text numberOfLines={1} style={styles.author}>
-                        {activity.name}
-                      </Text>
+                      <View style={styles.row}>
+                        <View style={{ flex: 1, marginRight: 10 }}>
+                          <Text style={styles.modalText}>{activity.name}</Text>
+                          {/* {activity.location.address && (
+                            <Text>{activity.location.address}</Text>
+                          )} */}
+                          {activity.location.citystate && (
+                            <View
+                              style={{
+                                backgroundColor: "lightblue",
+                                alignSelf: "flex-start",
+                                padding: 3,
+                                marginVertical: 5,
+                              }}
+                            >
+                              <Text
+                                numberOfLines={1}
+                                style={{ color: "darkblue" }}
+                              >
+                                {activity.location.citystate}
+                              </Text>
+                            </View>
+                          )}
+                          {activity.description && (
+                            <Text numberOfLines={3}>
+                              {activity.description}
+                            </Text>
+                          )}
+                        </View>
+                        <View style={{ width: 100, height: 100 }}>
+                          <Image
+                            key={activity.id}
+                            source={{
+                              uri: "https://images.unsplash.com/photo-1507608616759-54f48f0af0ee?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w2MTM3Mjd8MHwxfHNlYXJjaHw1fHxUcmF2ZWx8ZW58MHx8fHwxNzE2MTczNzc1fDA&ixlib=rb-4.0.3&q=80&w=400",
+                            }}
+                            style={{
+                              width: "100%",
+                              height: "100%",
+                              borderRadius: 8,
+                            }}
+                          />
+                          <View style={styles.likeContainer}>
+                            <Text style={styles.h4}>{activity.netUpvotes}</Text>
+                            <TouchableOpacity style={styles.heartIcon}>
+                              <Ionicons name={"heart"} color="red" size={18} />
+                            </TouchableOpacity>
+                          </View>
+                        </View>
+                      </View>
                     </TouchableOpacity>
-                  </View>
-                );
-              })}
-          </View>
-        </ScrollView>
+                  );
+                })
+              ) : (
+                <Text style={{ fontSize: 18 }}>
+                  Visit Suggestion page to upvote activities!
+                </Text>
+              )}
+            </View>
+          </ScrollView>
+        </View>
       </SafeAreaView>
     </Modal>
   );
@@ -102,6 +162,25 @@ const AddActivityToItinerary = ({
 export default AddActivityToItinerary;
 
 const styles = StyleSheet.create({
+  h1: {
+    fontWeight: "600",
+    fontSize: 18,
+  },
+  modalText: {
+    fontSize: 18,
+    overflow: "hidden",
+    fontWeight: "500",
+  },
+  h4: {
+    color: "black",
+    fontSize: 14,
+    fontWeight: "500",
+    lineHeight: 22,
+  },
+  p: {
+    fontSize: 12,
+    color: "white",
+  },
   row: {
     flexDirection: "row",
     alignItems: "center",
@@ -118,18 +197,21 @@ const styles = StyleSheet.create({
     fontWeight: "500",
     fontSize: 18,
   },
-  imageContainer: {
-    flexWrap: "wrap",
-    flexDirection: "row",
-    padding: 5,
-  },
-  author: {
-    marginLeft: 5,
-    marginBottom: 5,
-    textDecorationLine: "underline",
-  },
   note: {
     alignSelf: "center",
     fontSize: 15,
+  },
+  likeContainer: {
+    position: "absolute",
+    top: 10,
+    right: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "rgba(255, 255, 255, 1)",
+    padding: 5,
+    borderRadius: 5,
+  },
+  heartIcon: {
+    marginLeft: 5,
   },
 });
