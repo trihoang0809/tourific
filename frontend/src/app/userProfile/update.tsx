84c2414889ba3dafae5db62a702978c808290b13
Duy Kha <duykha121314888@gmail.com>
2024-06-17 09:48:23 -0500

    Commented Firebase parts

--- frontend/src/app/userProfile/update.tsx
+++ frontend/src/app/userProfile/update.tsx
@@ -1,9 +1,8 @@
 import React from "react";
-import { View } from "react-native";
-import { UserProfileCreate } from "@/screens/UserProfile/UserProfileCreate";
-// import UserProfileCreate from './userProfile/create';
+import { UserProfileEdit } from "@/screens/UserProfile/UserProfileEdit";
+
 const UpdatePage = () => {
-  return <UserProfileCreate id="6659f8e36ac8b75586fa7d73" method="PUT" />;
+  return <UserProfileEdit id="6659f8e36ac8b75586fa7d73" method="PUT" />;
 };
 
 export default UpdatePage;
