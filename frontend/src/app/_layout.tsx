b1a8fd8f39e967cb6cae35bed3c830ac21a4378c
Van Nguyen <87792873+yourvivian@users.noreply.github.com>
2024-06-23 20:20:22 -0700

    Fix all app navigation and alert messages

--- frontend/src/app/_layout.tsx
+++ frontend/src/app/_layout.tsx
@@ -8,7 +8,7 @@ const RootNavigation = () => {
         name='trips'
         options={{ title: 'Trip', headerShown: false }}
       />
-      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="index" options={{title: 'Home', headerShown: false }} />
     </Stack>
   );
 };
