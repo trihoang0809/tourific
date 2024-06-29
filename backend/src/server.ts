c71c96efdb1f5204a2306d380c0b2804d3106465
Van Nguyen <87792873+yourvivian@users.noreply.github.com>
2024-06-29 08:53:33 -0700

    Socket server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,9 +8,25 @@ const cors = require("cors");
 const port = process.env.PORT || 3000;
 const LOCAL_HOST_URL = process.env.LOCAL_HOST_URL;
 const prisma = new PrismaClient();
+const { scheduleTripNotifications } = require("./services/tripNotification");
+import { createServer } from "http";
+import { Server as SocketIOServer, Socket } from "socket.io";
 
 app.use(express.json());
 app.use(cors());
+
+// Create HTTP server
+const server = createServer(app);
+
+// Create Socket.IO server
+const io = new SocketIOServer(server, {
+  cors: {
+    origin: `http://${LOCAL_HOST_URL}:8081`,
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true,
+  },
+});
 
 // Get all trips
 app.use("/trips", TripRouter);
@@ -21,9 +37,19 @@ app.use("/user", UserProfile);
 // Connect to MongoDB via Prisma and start the server
 const startServer = async () => {
   await connect();
-  app.listen(port, () => {
-    console.log(`Server running at http://${LOCAL_HOST_URL}:${port}`);
-  });
+  // app.listen(port, () => {
+  //   console.log(`Server running at http://${LOCAL_HOST_URL}:${port}`);
+  // });
+  server.listen(port, () => console.log(`Server running at http://${LOCAL_HOST_URL}:${port}`));
+  scheduleTripNotifications(io);
 };
 
 startServer();
+
+// Add messages when sockets open and close connections
+io.on("connection", (socket: Socket) => {
+  console.log(`[${socket.id}] socket connected`);
+  socket.on("disconnect", (reason) => {
+    console.log(`[${socket.id}] socket disconnected - ${reason}`);
+  });
+});
