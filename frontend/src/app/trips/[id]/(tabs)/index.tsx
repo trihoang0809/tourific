8302f3c75de45af3dc2c7ad5b975780390bcd90e
Duy Kha <duykha121314888@gmail.com>
2024-06-30 11:24:10 -0500

    Merge kha/TSPalgo

--- frontend/src/app/trips/[id]/(tabs)/index.tsx
+++ frontend/src/app/trips/[id]/(tabs)/index.tsx
@@ -181,7 +181,7 @@ const TripDetailsScreen = () => {
                 {schedule.route.map((activity: any, id: number) => (
                   <Text key={id}>{id + " " + activity.name}</Text>
                 ))}
-                <Text>{schedule.cost}</Text>
+                <Text>{"total distance: " + schedule.cost}</Text>
               </Pressable>
             </View>
           </View>
