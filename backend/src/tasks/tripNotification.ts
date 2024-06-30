da6322f1b6ff32dedba62c46b402c74e1a9ff813
Van Nguyen <87792873+yourvivian@users.noreply.github.com>
2024-06-29 20:24:02 -0700

    Cron job to send notification to users to remind to upvote activities

--- backend/src/tasks/tripNotification.ts
+++ backend/src/tasks/tripNotification.ts
@@ -1,5 +1,6 @@
 const cron = require("node-cron");
 import { sendTomorrowTripsStartNotifications } from "../services/tripStartNotification";
+import { sendUpvoteMoreActivitiesNotification } from "../services/upvoteActivitiesNotification";
 
 function scheduleTripNotifications() {
   // For production, run this cron job every day at midnight UTC
@@ -8,7 +9,6 @@ function scheduleTripNotifications() {
   // For testing, run this cron job every 20 seconds
   // You can modify this line to schedule the frequency of the task
   cron.schedule("*/20 * * * * *", async () => {
-  
     try {
       await sendTomorrowTripsStartNotifications();
       console.log("Successfully scheduled task to notify tomorrow's trip");
@@ -16,5 +16,17 @@ function scheduleTripNotifications() {
       console.error("Error in schedule task to notify tomorrow's trip:", error);
     }
   });
+
+  cron.schedule("*/20 * * * * *", async () => {
+    // For testing, run every 20 seconds
+    // cron.schedule("0 0 * * 0", async () => { // For production: Run at midnight (0 hours and 0 minutes) on Sunday
+    try {
+      await sendUpvoteMoreActivitiesNotification();
+      console.log("Successfully scheduled task to remind users to upvote more activities");
+    } catch (error) {
+      console.error("Error in schedule task to remind users to upvote more activities:", error);
+    }
+  });
 }
+
 module.exports = { scheduleTripNotifications };
