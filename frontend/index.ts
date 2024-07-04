efd0b42d9c742682d49114cfce4d686a7b56acb1
Duy Kha <duykha121314888@gmail.com>
2024-07-03 21:27:43 -0500

    Fix warning

--- frontend/index.ts
+++ frontend/index.ts
@@ -1,1 +1,23 @@
 import "expo-router/entry";
+
+import { registerTranslation } from "react-native-paper-dates";
+registerTranslation("en", {
+  save: "Save",
+  selectSingle: "Select date",
+  selectMultiple: "Select dates",
+  selectRange: "Select period",
+  notAccordingToDateFormat: (inputFormat) =>
+    `Date format must be ${inputFormat}`,
+  mustBeHigherThan: (date) => `Must be later then ${date}`,
+  mustBeLowerThan: (date) => `Must be earlier then ${date}`,
+  mustBeBetween: (startDate, endDate) =>
+    `Must be between ${startDate} - ${endDate}`,
+  dateIsDisabled: "Day is not allowed",
+  previous: "Previous",
+  next: "Next",
+  typeInDate: "Type in date",
+  pickDateFromCalendar: "Pick date from calendar",
+  close: "Close",
+  hour: "",
+  minute: "",
+});
