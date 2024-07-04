6130d57a38c3631bcbaf780400456cd571eb11d8
Van Nguyen <87792873+yourvivian@users.noreply.github.com>
2024-07-03 20:08:49 -0700

    mock data for notification recipient

---
+++ frontend/src/mock-data/forNotification.tsx
@@ -0,0 +1,27 @@
+export const user0 = {
+  id: "6683f5179867bc7464c5c7bd",
+  userName: "tranguv",
+  firstName: "Trang",
+  notificationToken: "ExponentPushToken[fZTv_qIqpMWU_NZ7DJl0QZ]", // Van's ipad
+};
+
+export const user2 = {
+  id: "6685fed69986894c1ac08b53",
+  userName: "testing",
+  firstName: "Tri",
+  notificationToken: "ExponentPushToken[KL2mQvAmxLKAOJvP_iiTlO]", // Van's phone
+};
+
+export const user3 = {
+  id: "6684ef6ca3ea75b07fde4608",
+  userName: "messi",
+  firstName: "Leo",
+  notificationToken: "ExponentPushToken[fZTv_qIqpMWU_NZ7DJl0QZ]", // Van's ipad
+};
+
+export const mockTrip = {
+  name: "Tham ba ngoai o Canada",
+  location: {
+    citystate: "Toronto, CA",
+  },
+};
