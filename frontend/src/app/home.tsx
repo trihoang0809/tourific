1853499f91f14d04aba1770716f3dd9d11cae7c2
Van Nguyen <87792873+yourvivian@users.noreply.github.com>
2024-07-05 14:36:00 -0700

    remove push token alert

--- frontend/src/app/home.tsx
+++ frontend/src/app/home.tsx
@@ -151,7 +151,7 @@ async function registerForPushNotificationsAsync() {
       token = `${e}`;
     }
   } else {
-    alert("Must use physical device for Push Notifications");
+    // alert("Must use physical device for Push Notifications");
   }
 
   return token;
