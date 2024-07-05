f8604ffdc9d21bfc9b852bfd301b32fafe3cf084
Van Nguyen <87792873+yourvivian@users.noreply.github.com>
2024-07-04 22:05:55 -0700

    fetch number of noti from homepage

--- frontend/src/components/HomeScreenHeader.tsx
+++ frontend/src/components/HomeScreenHeader.tsx
@@ -9,10 +9,76 @@ import { UserProps } from "../types";
 import { Ionicons } from "@expo/vector-icons";
 import { router } from "expo-router";
 import React, { useEffect, useState } from "react";
-import { fetchInitialNotificationCount } from "@/utils/AsyncStorageUtils";
+import { fetchInitialNotificationCount, storeNotificationCount } from "@/utils/AsyncStorageUtils";
+import { FriendRequest, Notification, TripMembership } from "@/types";
+const EXPO_PUBLIC_HOST_URL = process.env.EXPO_PUBLIC_HOST_URL;
 
 export const HomeScreenHeader: React.FC<UserProps> = ({ user }) => {
   const [notificationCount, setNotificationCount] = useState<number>(0);
+  const [requests, setRequests] = useState<FriendRequest[]>([]);
+  const [invitations, setInvitations] = useState<TripMembership[]>([]);
+
+  useEffect(() => {
+    getFriendRequests();
+    getTripInvitations();
+  }, []);
+
+  useEffect(() => {
+    const totalNotifications = requests.length + invitations.length;
+    console.log("totalnoti: ", totalNotifications)
+    storeNotificationCount(totalNotifications)
+    setNotificationCount(totalNotifications)
+  }, [requests, invitations]);
+
+  const getFriendRequests = async () => {
+    try {
+      const response = await fetch(
+        `http://${EXPO_PUBLIC_HOST_URL}:3000/user/friend/pending-requests`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        },
+      );
+      if (!response.ok) {
+        console.error("Failed to fetch friend requests");
+      }
+      const data = await response.json();
+      setRequests(data);
+      console.log("Friend requests fetch:", data);
+    } catch (error: any) {
+      console.error(
+        "Error fetching pending friend requests:",
+        error.toString(),
+      );
+    }
+  };
+
+  const getTripInvitations = async () => {
+    try {
+      const response = await fetch(
+        `http://${EXPO_PUBLIC_HOST_URL}:3000/trips/invite/all-received`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        },
+      );
+      if (!response.ok) {
+        console.error("Failed to fetch trip invitations");
+      }
+      const data = await response.json();
+      setInvitations(data);
+      console.log("Trip invitations fetch:", data);
+    } catch (error: any) {
+      console.error(
+        "Error fetching pending trip invitations:",
+        error.toString(),
+      );
+    }
+  };
 
   useEffect(() => {
     const interval = setInterval(async () => {
